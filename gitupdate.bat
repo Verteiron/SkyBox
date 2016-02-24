d34e348d8d5963d0b1b6ae40032eb0185aa8ff43
Verteiron <verteiron@gmail.com>
2016-02-24 17:17:22 -0600

    Copy in c++ changes as well.

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,5 +1,8 @@
 @echo off
 SET SOURCEDIR=C:\Program Files (x86)\Steam\steamapps\common\skyrim\Data
 SET TARGETDIR=%USERPROFILE%\Dropbox\SkyrimMod\SkyBox\dist\Data
+SET DEPSOURCEDIR=D:\Projects\SKSE\SuperStash\SuperStash
+SET DEPTARGETDIR=%USERPROFILE%\Dropbox\SkyrimMod\SkyBox\dist\dep\SuperStash\SuperStash
 
 xcopy /E /U /Y "%SOURCEDIR%\*" "%TARGETDIR%\"
+xcopy /E /D /U /Y "%DEPSOURCEDIR%\*" "%DEPTARGETDIR%\"
